88f0d47014687644bcd2474588e4e33f130bac08
mariacperon <mariaclaraperon@gmail.com>
2022-02-11 13:39:10 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Bemvindo = (props) => {
+    return (
+        <div>
+            <h2>Bem-vindo(a), {props.nome}</h2>
+            <h3>Tenho {props.idade} anos</h3>
+        </div> 
+    )}
+
+function App(){
+    return (
+        <div> 
+            Olá, mundo!
+            <Bemvindo nome="Matheus" idade="19"/>
+            <Bemvindo nome="Maria" idade="21"/>
+            <h1>Curso react</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
