59f88324fbaba0120adf5950eb99dd2458eee101
mariacperon <mariaclaraperon@gmail.com>
2022-02-11 15:03:14 -0300

    aula 36

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {Component} from 'react'
 
 class App extends Component{
@@ -11,22 +10,6 @@ class App extends Component{
         }
         this.aumentar = this.aumentar.bind(this)
         this.diminur = this.diminur.bind(this)
-    }
-
-    aumentar(){
-        let state = this.state
-        state.contador += 1
-        this.setState(state)
-    }
-
-    diminur(){
-        let state = this.state
-        if(state.contador === 0){
-            alert('Não é permitido ter contadores negativos.')
-            return
-        }
-        state.contador -= 1
-        this.setState(state)
     }
 
     render(){
