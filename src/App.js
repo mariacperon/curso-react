db6546ca90f0dcd60420aaf4938270e98696d19b
mariacperon <mariaclaraperon@gmail.com>
2022-02-18 13:55:21 -0300

    aula 41

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react'
-import Membro from './components/Membro'
+import Feed from './components/Feed';
 
 class App extends Component{
     
+    constructor(props){
+        super(props);
+        this.state = {
+            feed:[
+                {id: 1, username: 'Matheus', curtidas: 1, comentarios: 2},
+                {id: 2, username: 'Lucas', curtidas: 120, comentarios: 24},
+                {id: 1, username: 'Amanda', curtidas: 30, comentarios: 12}
+            ]
+        }
+    }
+
     render(){
         return (
             <div> 
-                <Membro nome="visitante"/>
+                {this.state.feed.map((item) => {
+                    return(
+                        <Feed key={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
+                    )
+                })}
             </div>
         )
     }
