e2d1729f5b31410dccf08415283b2e8c3f951eb1
mariacperon <mariaclaraperon@gmail.com>
2022-03-09 15:34:11 -0300

    aula 48

--- src/App.js
+++ src/App.js
@@ -1,61 +1,32 @@
-import React, {Component} from 'react'
-import './style.css'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/alt-text */
+import React, {useState} from 'react'
 
-class App extends Component{
+function App(){
 
-    constructor(props){
-        super(props)
-        this.state = {
-            numero: 0,
-            botao: 'VAI'
-        }
-        this.timer = null;
-        this.vai = this.vai.bind(this)
-        this.limpar = this.limpar.bind(this)
+    const [tarefas, setTarefas] = useState([
+        'Pagar a conta de luz',
+        'Estudar React Hooks'
+    ])
+
+    const [input, setInput] = useState('')
+
+    function handleAdd(){
+        setTarefas([...tarefas, input])
+        setInput('')
     }
 
-    vai(){
-        let state = this.state
-        if(this.timer !== null){
-            clearInterval(this.timer)
-            this.timer = null
-            state.botao = 'VAI'
-        }else{
-            this.timer = setInterval(() => {
-                let state = this.state
-                state.numero += 0.1
-                this.setState(state)
-            }, 100)
-            state.botao = 'PAUSA'
-        }  
-
-        this.setState(state)
-    }
-
-    limpar(){
-        if(this.timer !== null){
-            clearInterval(this.timer)
-            this.timer = null
-        }
-
-        let state = this.state
-        state.numero = 0
-        state.botao = 'VAI'
-        this.setState(state)
-    }
-    
-    render(){
-        return (
-            <div className='container'> 
-                <img src={require('./assets/cronometro.png')} className='img'/>
-                <a className='timer'>{this.state.numero.toFixed(1)}</a>
-                <div className='areaBtn'>
-                    <a className='botao' onClick={this.vai}>{this.state.botao}</a>
-                    <a className='botao' onClick={this.limpar}>LIMPAR</a>
-                </div>
-            </div>
-        )
-    }
+    return (
+        <div> 
+            <button type='button' onClick={handleAdd}>Adicionar</button>
+            <input type='text' value={input} onChange={(e) => setInput(e.target.value)}></input>
+            <ul>
+                {tarefas.map(tarefa => (
+                    <li ke>{tarefa}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App
