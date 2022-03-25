2444b06274d0a8b8465402755a51d89c21c6276d
mariacperon <mariaclaraperon@gmail.com>
2022-03-25 16:41:05 -0300

    aula 53

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React, {useCallback, useEffect, useMemo, useState} from 'react'
+import './style.css'
 
 //https://sujeitoprogramador.com/rn-api/?api=posts
 
+
+
 function App(){
-    const [nutri, setNutri] = useState([])
+    const[nutri, setNutri] = useState()
 
     useEffect(() => {
         function loadAPI(){
             let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
-            fetch(url).then((r) => r.json()).then((json) => {
+            fetch(url)
+            .then((r) => r.json())
+            .then((json) => {
                 setNutri(json)
             })
         }
@@ -19,10 +24,23 @@ function App(){
     }, [])
 
     return (
-        <div className="container"> 
+        <div className='container'>
             <header>
-                <h1>react nutri</h1>
+                <strong>React Nutri</strong>
             </header>
+
+            {nutri.map((item) => {
+                return(
+                    <article key={item.id} className="post">
+                        <strong className="titulo">{item.titulo}</strong>
+                        <img src={item.capa} alt={item.titulo} className="capa"/>
+                        <p className="subtitulo">
+                            {item.substitulo}
+                        </p>
+                        <a className="botao">Acessar</a>
+                    </article>
+                )
+            })}
         </div>
     )
 }
